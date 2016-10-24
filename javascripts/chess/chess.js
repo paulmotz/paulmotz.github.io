777a0736d7daa5aa3a59da8bf04857eaaccee098
Paul Motz <pauljmotz@gmail.com>
2016-10-24 10:45:52 -0400

    combine drawWhite and drawBlack methods

--- javascripts/chess/chess.js
+++ javascripts/chess/chess.js
@@ -13,11 +13,55 @@ $(document).ready(function() {
 	var $board = $('#chessboard');
 
 	// TODO: what if height !== width?
+	// visual/layout variables
 	var height = parseInt($board.css('height'));
 	var width = parseInt($board.css('width'));
 	var squareSize = height/10;
 	var lineWidth = 5;
+
 	var markedSquares = new Set();
+
+	var board = $board[0];
+	var ctx = board.getContext('2d');
+	var files = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ''];
+	var ranks = ['', '1', '2', '3', '4', '5', '6', '7', '8', ''];
+
+	// game/logic variables
+
+	// the two players' colors
+	var colors = ['w', 'b'];
+
+	var pieceNames = {'B' : 'Bishop', 'N' : 'Knight', 'K' : 'King', 'P' : 'Pawn', 'Q' : 'Queen', 'R' : 'Rook'};
+	var pieceSymbols = {'Bishop' : 'B', 'Knight' : 'N', 'King' : 'K', 'Pawn' : 'P', 'Queen' : 'Q', 'Rook' : 'R'};
+	var pieceCount = {'B': 2, 'N': 2, 'K': 1, 'P': 8, 'Q': 1, 'R': 2};
+
+	// https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
+	var pieceSymbols = {'bB': '♝', 'bN': '♞', 'bK': '♚', 'bP': '♟', 'bQ': '♛', 'bR': '♜', 'wB': '♗', 'wN': '♘', 'wK': '♔', 'wP': '♙', 'wQ': '♕', 'wR': '♖'};
+
+	// kings and queens have arrays of length 1 for convenience in later methods
+	var pieceStartingPositions = {'wB' : [[3, 1], [6, 1]],
+									  'wN' : [[2, 1], [7, 1]],
+									  'wK' : [[5, 1]],
+									  'wP' : [[1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2]],
+									  'wQ' : [[4, 1]],
+									  'wR' : [[1, 1], [8, 1]],
+									  'bB' : [[3, 8], [6, 8]],
+									  'bN' : [[2, 8], [7, 8]],
+									  'bK' : [[5, 8]],
+									  'bP' : [[1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7], [8, 7]],
+									  'bQ' : [[4, 8]],
+									  'bR' : [[1, 8], [8, 8]]
+									};
+
+	// represent all pieces as entries in arrays for dynamic access (kings could be single entry, I guess)
+	var allPieces = {'wB' : [], 'wN' : [], 'wK' : [], 'wP' : [], 'wQ' : [], 'wR' : [], 'bB' : [], 'bN' : [], 'bK' : [], 'bP' : [], 'bQ' : [], 'bR' : [] };
+
+	// create an array of 64 square objects
+	var gameBoard = initializeBoard();
+
+	var whiteDown = true;
+
+	var occupiedSquares = new Set();
 
 	$board.on('click', function(e) {
 		var x = e.offsetX;
@@ -33,114 +77,20 @@ $(document).ready(function() {
 			ctx.stroke();
 			var index = squareToIndex(square);
 			console.log(gameBoard[index - 1]);
-			// for (var ms of markedSquares.keys()) {
-			// 	var oldSquare = indexToSquare(ms);
-			// 	var oldCoordinates = getCoordinates(oldSquare[0], oldSquare[1]);
-			// 	console.log(oldSquare);
-			// 	if ((oldSquare[0] + oldSquare[1]) % 2 === 1) {
-			// 		ctx.fillStyle = "#999";
-			// 	}
-			// 	else {
-			// 		ctx.fillStyle = "#444";
-			// 	}
-			// 	ctx.fillRect(oldSquare[0] * squareSize, (9 - oldSquare[1]) * squareSize, squareSize, squareSize);
-			// 	markedSquares.delete(ms);
-			// }
-			// markedSquares.add(index);
-			// console.log(markedSquares);
 		}		
-	});
-
-	$('input[type=radio]').change(function() {
-		if (this.value === 'white') {
-			whiteDownNextGame = true;
-		}
-		else {
-			whiteDownNextGame = false;
-		}
 	});
 
 	$('.btn').on('click', function() {
 		if ($('#radio-white').is(':checked')) {
 			whiteDown = true;
-			drawWhite();
 		}
 		else {
 			whiteDown = false;
-			drawBlack();
 		}
+		drawBoard();
+		// newGame();
 
 	});
-
-	var board = $board[0];
-	var ctx = board.getContext('2d');
-
-	// piece objects
-	// king objects don't need to be arrays, I guess
-	var wB = [],
-		wN = [],
-		wK = [],
-		wP = [],
-		wQ = [],
-		wR = [];
-		bB = [],
-		bN = [],
-		bK = [],
-		bP = [],
-		bQ = [],
-		bR = [];
-
-	var colors = ['w', 'b'];
-
-	var whitePieces = {'B' : wB, 'N' : wN, 'K' : wK, 'P' : wP, 'Q' : wQ, 'R' : wR};
-	var blackPieces = {'B' : bB, 'N' : bN, 'K' : bK, 'P' : bP, 'Q' : bQ, 'R' : bR};
-
-	var allPieces = {'wB' : [], 'wN' : [], 'wK' : [], 'wP' : [], 'wQ' : [], 'wR' : [], 'bB' : [], 'bN' : [], 'bK' : [], 'bP' : [], 'bQ' : [], 'bR' : [] };
-
-	var files = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', ''];
-	var ranks = ['', '1', '2', '3', '4', '5', '6', '7', '8', ''];
-
-	var pieceNames = {'B' : 'Bishop', 'N' : 'Knight', 'K' : 'King', 'P' : 'Pawn', 'Q' : 'Queen', 'R' : 'Rook'};
-	var pieceSymbols = {'Bishop' : 'B', 'Knight' : 'N', 'King' : 'K', 'Pawn' : 'P', 'Queen' : 'Q', 'Rook' : 'R'};
-	var pieceCount = {'B': 2, 'N': 2, 'K': 1, 'P': 8, 'Q': 1, 'R': 2};
-
-	// https://en.wikipedia.org/wiki/Chess_symbols_in_Unicode
-	// var whitePieceSymbols = {'B': '♗', 'N': '♘', 'K': '♔', 'P': '♙', 'Q': '♕', 'R': '♖'};
-	// var blackPieceSymbols = {'B': '♝', 'N': '♞', 'K': '♚', 'P': '♟', 'Q': '♛', 'R': '♜'};
-	var pieceSymbols = {'bB': '♝', 'bN': '♞', 'bK': '♚', 'bP': '♟', 'bQ': '♛', 'bR': '♜', 'wB': '♗', 'wN': '♘', 'wK': '♔', 'wP': '♙', 'wQ': '♕', 'wR': '♖'};
-
-	// kings and queens have arrays of length 1 for convenience in later methods
-	var pieceStartingPositions = {'bB' : [[3, 8], [6, 8]],
-									  'bN' : [[2, 8], [7, 8]],
-									  'bK' : [[5, 8]],
-									  'bP' : [[1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7], [8, 7]],
-									  'bQ' : [[4, 8]],
-									  'bR' : [[1, 8], [8, 8]],
-									  'wB' : [[3, 1], [6, 1]],
-									  'wN' : [[2, 1], [7, 1]],
-									  'wK' : [[5, 1]],
-									  'wP' : [[1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2]],
-									  'wQ' : [[4, 1]],
-									  'wR' : [[1, 1], [8, 1]]
-									};
-
-	// var whitePieceStartingPositions = {'wB' : [[3, 1], [6, 1]],
-	// 								   'wN' : [[2, 1], [7, 1]],
-	// 								   'wK' : [[5, 1]],
-	// 								   'wP' : [[1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2]],
-	// 								   'wQ' : [[4, 1]],
-	// 								   'wR' : [[1, 1], [8, 1]]};
-
- //   var blackPieceStartingPositions = {'bB' : [[3, 8], [6, 8]],
-	// 								  'bN' : [[2, 8], [7, 8]],
-	// 								  'bK' : [[5, 8]],
-	// 								  'bP' : [[1, 7], [2, 7], [3, 7], [4, 7], [5, 7], [6, 7], [7, 7], [8, 7]],
-	// 								  'bQ' : [[4, 8]],
-	// 								  'bR' : [[1, 8], [8, 8]]};
-
-
-
-	// var startingPositions = [whitePieceStartingPositions, blackPieceStartingPositions];
 
 	// initilize all pieces
 	for (var color in colors) {
@@ -172,18 +122,7 @@ $(document).ready(function() {
 			}
 		}
 	}
-
-	var occupiedSquares = new Set();
-
-	// console.log(bP[0].moves);
-	// console.log(whitePieces);
-
-	var gameBoard = initializeBoard();
-
-	var whiteDown = true;
-	var whiteDownNextGame = true;
-
-	drawWhite();
+	drawBoard();
 
 	// console.log(occupiedSquares);
 
@@ -204,57 +143,9 @@ $(document).ready(function() {
 	}
 
 	/**
-	 * Draw the board with white at the bottom
+	 * Draw the board
 	 */
-	function drawWhite() {
-
-		whiteDown = true;
-		
-		// redraw border to overwrite existing text
-		ctx.fillStyle = "#000";
-		ctx.fillRect(0, 0, squareSize * 10, squareSize * 10);
-
-		ctx.textAlign = "center";
-		ctx.textBaseline = "middle"; 
-		ctx.font = "20px serif";
-
-		for (var r = 0; r < 10; r++) {
-			for (var f = 0; f < 10; f++) {
-
-				// rank falls off the board 
-				if (r === 0 || r === 9) {
-					ctx.fillStyle = "#FFF";
-					ctx.fillText(ranks[9-f], (r + 0.5) * squareSize, (f + 0.5) * squareSize);
-				}
-
-				// file falls off the board
-				else if (f === 0 || f === 9) {
-					ctx.fillStyle = "#FFF";
-					ctx.fillText(files[r], (r + 0.5) * squareSize, (f + 0.5) * squareSize);
-				}
-
-				// draw square
-				else {
-					if ((r + f) % 2 === 1) {
-						ctx.fillStyle = "#444";
-					}
-					else {
-						ctx.fillStyle = "#999";
-					}
-					ctx.fillRect(f * squareSize, r * squareSize, squareSize, squareSize);
-				}
-			}
-		}
-		drawPieces();
-	}
-
-	/**
-	 * Draw the board with black at the bottom
-	 */
-
-	function drawBlack() {
-
-		whiteDown = false;
+	function drawBoard() {
 
 		// redraw border to overwrite existing text
 		ctx.fillStyle = "#000";
@@ -267,16 +158,19 @@ $(document).ready(function() {
 		for (var r = 0; r < 10; r++) {
 			for (var f = 0; f < 10; f++) {
 
-				// rank falls off the board 
+				var fileIndex = whiteDown ? f : 9 - f;
+				var rankIndex = whiteDown ? 9 - r : r;
+
+				// rank falls off the board, write the file name
 				if (r === 0 || r === 9) {
 					ctx.fillStyle = "#FFF";
-					ctx.fillText(ranks[f], (r + 0.5) * squareSize, (f + 0.5) * squareSize);
+					ctx.fillText(files[fileIndex], (f + 0.5) * squareSize, (r + 0.5) * squareSize);
 				}
 
-				// file falls off the board
+				// file falls off the board, writethe rank name
 				else if (f === 0 || f === 9) {
 					ctx.fillStyle = "#FFF";
-					ctx.fillText(files[9-r], (r + 0.5) * squareSize, (f + 0.5) * squareSize);
+					ctx.fillText(ranks[rankIndex], (f + 0.5) * squareSize, (r + 0.5) * squareSize);
 				}
 
 				// draw square
@@ -299,7 +193,6 @@ $(document).ready(function() {
 	 */
 
 	function drawPieces() {
-
 		for (var pieceType in allPieces) {
 			var pieces = allPieces[pieceType];
 			// console.log(pieces);
@@ -325,18 +218,6 @@ $(document).ready(function() {
 			}
 		}
 	}
-
-	/**
-	 * Maps the rank and file of a square to an integer that is unique among other squares
-	 * @param {number} file - the square's file: 1 - 8
-	 * @param {number} rank - the square's rank: 1 - 8
-	 * @return {number} index - the indicex of the square: 1 - 64
-	 */
-
-	// function squareToIndex(file, rank) {
-	// 	console.log('hi');
-	// 	return (rank - 1) * 8 + file;
-	// }
 
 	/**
 	 * Maps the rank and file of a square to an integer that is unique among other squares
