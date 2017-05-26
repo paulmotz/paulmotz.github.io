7843855d362f54407ce2c4390da72a74585eb639
Paul Motz <pauljmotz@gmail.com>
2017-05-25 20:52:44 -0400

    update running data

---
+++ javascripts/results.js
@@ -0,0 +1,403 @@
+[Log] Object (nhlCities.js, line 277)
+
+Anaheim Ducks: ["Rocco Grimaldi", "Ian McCoshen"] (2)
+
+Arizona Coyotes: [] (0)
+
+Boston Bruins: Array (12)
+0 "Noel Acciari"
+1 "Nick Bonino"
+2 "Brian Boyle"
+3 "Brian Flynn"
+4 "Garnet Hathaway"
+5 "Ron Hainsey"
+6 "Kevin Hayes"
+7 "Charlie Coyle"
+8 "Kevin Rooney"
+9 "Zach Sanford"
+10 "Brian Strait"
+11 "Matt Grzelcyk"
+
+Array Prototype
+
+Buffalo Sabres: ["Miles Wood", "Marcus Foligno", "Justin Bailey", "Sean Malone", "Nick Foligno", "Patrick Kane", "Dustin Brown"] (7)
+
+Calgary Flames: Array (18)
+0 "Brayden Point"
+1 "Patrick Eaves"
+2 "Taylor Hall"
+3 "Jay Beagle"
+4 "Ty Rattie"
+5 "Josh Morrissey"
+6 "Joe Colborne"
+7 "Thomas Hickey"
+8 "Justin Dowling"
+9 "Hunter Shinkaruk"
+10 "Mike Green"
+11 "Alex Nylander"
+12 "William Nylander"
+13 "Shea Weber"
+14 "Cody Franson"
+15 "Connor Jones"
+16 "Patrick Marleau"
+17 "Mason Raymond"
+
+Array Prototype
+
+Carolina Hurricanes: ["Jared Boll"] (1)
+
+Chicago Blackhawks: ["Justin Abdelkader", "Luke Glendening", "Kevin Gravel", "John Hayden", "Mike Reilly", "Christian Fischer", "Vinnie Hinostroza"] (7)
+
+Colorado Avalanche: ["Nick Shore", "Jaccob Slavin", "Drew Shore"] (3)
+
+Columbus Blue Jackets: ["John-Michael Liles", "Jack Johnson", "Jack Roslovic"] (3)
+
+Dallas Stars: [] (0)
+
+Detroit Red Wings: Array (31)
+0 "Dylan Demelo"
+1 "Joe Thornton"
+2 "Drew Doughty"
+3 "Nazem Kadri"
+4 "Travis Konecny"
+5 "Andreas Athanasiou"
+6 "Sam Gagner"
+7 "Jeff Carter"
+8 "Bo Horvat"
+9 "Brian Campbell"
+10 "Nathan Beaulieu"
+11 "Matt Puempel"
+12 "Aaron Ekblad"
+13 "Matt Beleskey"
+14 "Matt Martin"
+15 "Zack Kassian"
+16 "Cam Fowler"
+17 "Reid Boucher"
+18 "T.J. Brodie"
+19 "Patrick Brown"
+20 "Ian Cole"
+21 "Andrew Copp"
+22 "Jeff Petry"
+23 "Austin Watson"
+24 "Steven Kampfer"
+25 "Seth Griffith"
+26 "Matt Hunwick"
+27 "Jordan Oesterle"
+28 "Matt Read"
+29 "Matt Greene"
+30 "Claude Giroux"
+
+Array Prototype
+
+Edmonton Oilers: Array (40)
+0 "Vernon Fiddler"
+1 "David Schlemko"
+2 "Jason Chimera"
+3 "Brendan Guhle"
+4 "Johnny Boychuk"
+5 "Jarome Iginla"
+6 "Kevin Connauton"
+7 "Deryk Engelland"
+8 "Brett Kulak"
+9 "Tyler Ennis"
+10 "Duncan Siemens"
+11 "Alex Petrovic"
+12 "Bryan Little"
+13 "Jared Spurgeon"
+14 "Brendan Gallagher"
+15 "John Quenneville"
+16 "Taylor Fedun"
+17 "Ryan Garbutt"
+18 "Dion Phaneuf"
+19 "Dillon Simpson"
+20 "Jay Bouwmeester"
+21 "Joe Morrow"
+22 "Matthew Benning"
+23 "Lance Bouma"
+24 "Rene Bourque"
+25 "Kyle Brodziak"
+26 "Daniel Carr"
+27 "Mark Pysyk"
+28 "Boyd Gordon"
+29 "Chandler Stephenson"
+30 "Luke Schenn"
+31 "Nelson Nogier"
+32 "Eric Gryba"
+33 "Brayden Schenn"
+34 "Taylor Leier"
+35 "Nick Holden"
+36 "Tyson Jost"
+37 "Colton Parayko"
+38 "Mark Letestu"
+39 "Linden Vey"
+
+Array Prototype
+
+Florida Panthers: ["Shayne Gostisbehere"] (1)
+
+Las Vegas Golden Knights: [] (0)
+
+Los Angeles Kings: ["Beau Bennett", "Matt Nieto", "Emerson Etem", "Shane Harper"] (4)
+
+Minnesota Wild: Array (28)
+0 "Zach Parise"
+1 "Matt Tennyson"
+2 "Paul Martin"
+3 "Nick Bjugstad"
+4 "David Backes"
+5 "Justin Braun"
+6 "Tyler Pitlick"
+7 "Joseph Labate"
+8 "Brock Boeser"
+9 "J.T. Brown"
+10 "Eric Staal"
+11 "Marc Staal"
+12 "Taylor Chorney"
+13 "Jordan Staal"
+14 "Robert Bortuzzo"
+15 "Tom Pyatt"
+16 "Jonny Brodzinski"
+17 "Derek Forbort"
+18 "Alex Goligoski"
+19 "Erik Johnson"
+20 "Tom Gilbert"
+21 "Matt Hendricks"
+22 "Michael Kapla"
+23 "Jake McCabe"
+24 "Joe Pavelski"
+25 "Jake Gardiner"
+26 "Seth Helgeson"
+27 "Jake Guentzel"
+
+Array Prototype
+
+Montreal Canadiens: Array (21)
+0 "Anthony Beauvillier"
+1 "Francois Beauchemin"
+2 "Kris Letang"
+3 "Alex Chiasson"
+4 "Mike Ribeiro"
+5 "Marc-Edouard Vlasic"
+6 "Mark Barberio"
+7 "Marco Scandella"
+8 "Alex Biega"
+9 "Patrice Bergeron"
+10 "Michael Bournival"
+11 "Gabriel Bourque"
+12 "Thomas Chabot"
+13 "Phillip Danault"
+14 "Frederick Gaudreau"
+15 "A.J. Greer"
+16 "Jonathan Huberdeau"
+17 "Charles Hudon"
+18 "Steve Ott"
+19 "Frederik Gauthier"
+20 "Yanni Gourde"
+
+Array Prototype
+
+Nashville Predators: [] (0)
+
+New Jersey Devils: ["Kenny Agostino"] (1)
+
+New York Islanders: ["Anthony Bitetto", "Patrick Cannone", "Kevin Labanc", "Sonny Milano", "Rob O'Gara"] (5)
+
+New York Rangers: ["Cam Atkinson", "Brandon Sutter"] (2)
+
+Ottawa Senators: Array (23)
+0 "Mark Borowiecki"
+1 "Mackenzie Weegar"
+2 "Marc Methot"
+3 "Paul Byron"
+4 "Nicholas Baptiste"
+5 "Erik Gudbranson"
+6 "Trevor Smith"
+7 "Jean-Gabriel Pageau"
+8 "Ryan Spooner"
+9 "Cody Ceci"
+10 "Zach Bogosian"
+11 "PA Parenteau"
+12 "Derick Brassard"
+13 "Calvin de Haan"
+14 "Eric Gelinas"
+15 "Jay McClement"
+16 "Scott Harrington"
+17 "Samuel Henley"
+18 "Ben Hutton"
+19 "Slater Koekkoek"
+20 "Remi Elie"
+21 "Cristoval Nieves"
+22 "Alex Tuch"
+
+Array Prototype
+
+Philadelphia Flyers: ["Johnny Gaudreau", "Bobby Ryan"] (2)
+
+Pittsburgh Penguins: ["Riley Barber", "Matt Bartkowski", "Brandon Saad", "Vincent Trocheck"] (4)
+
+San Jose Sharks: [] (0)
+
+St. Louis Blues: ["Clayton Keller"] (1)
+
+Tampa Bay Lightning: ["Ryan Hartman"] (1)
+
+Toronto Maple Leafs: Array (93)
+0 "Ben Chiarot"
+1 "Mark Jankowski"
+2 "Spencer Abbott"
+3 "Ben Harpur"
+4 "Darnell Nurse"
+5 "Ryan Ellis"
+6 "Tyler Gaudet"
+7 "Evan McEneny"
+8 "Josh Anderson"
+9 "Cory Conacher"
+10 "Josh Jooris"
+11 "Drake Caggiula"
+12 "Andy Andreoff"
+13 "Dan Renouf"
+14 "Brady Austin"
+15 "Bryan Bickell"
+16 "Josh Bailey"
+17 "Cody Bass"
+18 "Riley Sheahan"
+19 "Taylor Beck"
+20 "Ryan Johnston"
+21 "Brendan Gaunce"
+22 "Tyler Bertuzzi"
+23 "Cameron Gaunce"
+24 "Derek MacKenzie"
+25 "Sam Bennett"
+26 "Phillip Di Giuseppe"
+27 "Luke Gazdic"
+28 "Brandon Tanev"
+29 "Tom Wilson"
+30 "Mark Giordano"
+31 "Chris Kelly"
+32 "Freddie Hamilton"
+33 "Trevor Daley"
+34 "Brandon Pirri"
+35 "Frank Corrado"
+36 "Mike Cammalleri"
+37 "Zach Hyman"
+38 "Dougie Hamilton"
+39 "Chris Didomenico"
+40 "Evan Rodrigues"
+41 "Peter Holland"
+42 "P.K. Subban"
+43 "Barclay Goodrow"
+44 "Connor Brown"
+45 "Joshua Ho-Sang"
+46 "Jason Spezza"
+47 "Jamie Oleksiak"
+48 "Daniel Winnik"
+49 "Mike Liambas"
+50 "Mike Blunden"
+51 "Harry Zolnierczyk"
+52 "Joseph Cramarossa"
+53 "Chris Stewart"
+54 "Chris Tanev"
+55 "Gemel Smith"
+56 "Jeff Skinner"
+57 "Adam Pelech"
+58 "Casey Cizikas"
+59 "Reilly Smith"
+60 "Cal O'Reilly"
+61 "Andrew Cogliano"
+62 "Kevin Bieksa"
+63 "Steven Stamkos"
+64 "Mitchell Marner"
+65 "Joseph Blandisi"
+66 "Tanner Richard"
+67 "Jake Dotchin"
+68 "Rick Nash"
+69 "Ben Thomson"
+70 "Tyler Seguin"
+71 "Tyler Graovac"
+72 "Chris Terry"
+73 "Sean Monahan"
+74 "Brandon Montour"
+75 "Adam Henrique"
+76 "Mike Hoffman"
+77 "Kyle Quincey"
+78 "Kevin Klein"
+79 "Dennis Wideman"
+80 "Mark Scheifele"
+81 "Tanner Pearson"
+82 "Josh Leivo"
+83 "Jared McCann"
+84 "Joel Ward"
+85 "Matt Moulson"
+86 "Chris Neil"
+87 "Michael Sgarbossa"
+88 "Corey Perry"
+89 "Mike Fisher"
+90 "Kurtis Gabriel"
+91 "Joel Hanley"
+92 "Chris Tierney"
+
+Array Prototype
+
+Vancouver Canucks: Array (33)
+0 "Ryan Nugent-Hopkins"
+1 "Tyler Wotherspoon"
+2 "Karl Alzner"
+3 "Patrick Wiercioch"
+4 "Brandon Dubinsky"
+5 "Matt Carle"
+6 "Casey Bailey"
+7 "Nate Thompson"
+8 "Mathew Barzal"
+9 "Ben Street"
+10 "Jamie Benn"
+11 "Matt Irwin"
+12 "Jordie Benn"
+13 "Tyson Barrie"
+14 "Evander Kane"
+15 "Milan Lucic"
+16 "Ryan Johansen"
+17 "Troy Brouwer"
+18 "Morgan Rielly"
+19 "Jason Garrison"
+20 "Andrew Ladd"
+21 "Brad Hunt"
+22 "Colton Sissons"
+23 "Bracken Kearns"
+24 "Jordan Weal"
+25 "Nic Petan"
+26 "Justin Schultz"
+27 "Josh Gorges"
+28 "Dan Hamhuis"
+29 "Derek Grant"
+30 "Mark McNeill"
+31 "Shea Theodore"
+32 "Danton Heinen"
+
+Array Prototype
+
+Washington Capitals: [] (0)
+
+Winnipeg Jets: Array (19)
+0 "Garrett Mitchell"
+1 "Ryan Murray"
+2 "Matt Dumba"
+3 "Josh Archibald"
+4 "Tanner Glass"
+5 "Jordan Eberle"
+6 "Chris Kunitz"
+7 "Scott Hartnell"
+8 "Ryan Getzlaf"
+9 "Tyler Bozak"
+10 "Ryan White"
+11 "Matt Calvert"
+12 "Joel Edmundson"
+13 "Jordan Martinook"
+14 "Micheal Ferland"
+15 "Travis Hamonic"
+16 "Darren Helm"
+17 "Eric Fehr"
+18 "Byron Froese"
+
+Array Prototype
+
+Object Prototype
