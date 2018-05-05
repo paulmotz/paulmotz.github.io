f88f4f5d294b70423423b5791be3cb3ae23bab4e
Paul Motz <pauljmotz@gmail.com>
2018-05-05 00:10:22 -0400

    ES6ify running code, restyle tooltips.

--- scripts/running.js
+++ scripts/running.js
@@ -29,25 +29,15 @@ let r = d3.scaleSqrt()
 	.domain([0,10])
 	.range([1,10]);
 
-let monthDays = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-let monthAbr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-let monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-let months = [];
-
-for (let i = 0; i < monthDays.length; i++) {
-	months.push({"val" : monthDays[i].toString(), "key" : monthNames[i]})
-}
-
-months.map(function(d) { return d})
+const MONTH_DAYS = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+const MONTH_ABBREVIATIONS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 /**
  * Draws the chart using svg.
  * @param {Object[]} data - running data
  */
-
 function draw(data) {
-
 	runs = getRuns(data);
 
 	let tooltip = d3.select("body").append("div")   
@@ -56,23 +46,22 @@ function draw(data) {
 
 	let tooltipOffset = 28;
 
-    let tooltipTriangle = d3.select("body").append("div")
-    	.attr("class", "tooltip-triangle")
-    	.style("opacity", 0);
+  let tooltipTriangle = d3.select("body").append("div")
+  	.attr("class", "tooltip-triangle")
+  	.style("opacity", 0);
 
 	let triangleSize = 8;
 
 	let svg = d3.select('.svg').attr('width',width).attr('height',height);
 
 	// x-axis
-	svg.append('g').attr('transform', 'translate(0,' + plotHeight + ')').call(d3.axisBottom(x).tickValues(monthDays).tickFormat(function(d,i){ return monthAbr[i]}));
+	svg.append('g').attr('transform', 'translate(0,' + plotHeight + ')').call(d3.axisBottom(x).tickValues(MONTH_DAYS).tickFormat((d, i) => {
+		return MONTH_ABBREVIATIONS[i]
+	}));
 
 	// y-axis
-	svg.append('g').attr('transform', 'translate(' + margins.left + ',0)').call(d3.axisLeft(y).tickFormat(function(d) { 
-		let min = Math.floor(d / 1);
-		let sec = d % 1 * 60;
-		let filler = sec < 10 ? ':0' : ':';
-		return min + filler + sec;
+	svg.append('g').attr('transform', 'translate(' + margins.left + ',0)').call(d3.axisLeft(y).tickFormat((pace) => { 
+		return getPaceString(pace);
 	}));
 
 	svg.append('text')
@@ -91,10 +80,10 @@ function draw(data) {
 
 	// plot the data (an array) with the longest runs first (so that smaller circles (short runs) are drawn on top of larger ones) if it only contains the running data 
 	// plot the data (an object) in chronological order if there is aggregate yearly data
-	let years = [];
-	for (let run in runs) {
-		let thisYear = runs[run].date.getFullYear();
-		if (years.indexOf(thisYear) === -1) {
+	const years = [];
+	for (const run of runs) {
+		const thisYear = run.date.getFullYear();
+		if (!years.includes(thisYear)) {
 			years.push(thisYear);
 		}
 	}
@@ -103,105 +92,101 @@ function draw(data) {
 
 	// custon tooltips inspired by: 
 	// http://stackoverflow.com/questions/16256454/d3-js-position-tooltips-using-element-position-not-mouse-position
-	$("svg circle, svg polygon").on('mouseover', function(e) {
+	$("svg circle, svg polygon").on('mouseover', function (e) {
 		if (!dropdownExpanded()) {
-			let rect = this.getBoundingClientRect();
-			let xScroll = $(window).scrollLeft();
-			let yScroll = $(window).scrollTop();
-			let xPos = rect.left + rect.width / 2 + xScroll;
-			let yPos = rect.top + rect.height / 2 + yScroll;
-			tooltip.style("opacity", 1); 
-			tooltipTriangle.style("opacity", 1); 
-			$(tooltip._groups[0][0]).css("z-index", 999);
-			let color = this.getAttribute('fill');
-			color = (color.replace("0.3", "0.7"));
+			const rect = this.getBoundingClientRect();
+			const xScroll = $(window).scrollLeft();
+			const yScroll = $(window).scrollTop();
+			const xPos = rect.left + rect.width / 2 + xScroll;
+			const yPos = rect.top + rect.height / 2 + yScroll;
+			tooltip.style('opacity', 1); 
+			tooltipTriangle.style('opacity', 1); 
+			$(tooltip._groups[0][0]).css('z-index', 999);
+			// make the tooltip darker than the corresponding circle
+			const color = this.getAttribute('fill').replace("0.3", "0.7");
 			tooltip.html(this.getAttribute('title'));
-			let tooltipHeight = tooltip._groups[0][0].offsetHeight;
-			let tooltipWidth = tooltip._groups[0][0].offsetWidth;	
+			const tooltipHeight = tooltip._groups[0][0].offsetHeight;
+			const tooltipWidth = tooltip._groups[0][0].offsetWidth;	
 
-			let t = $(".tooltip-triangle");
+			const t = $(".tooltip-triangle");
 
 			let tooltipX;
 			let tooltipY;
 
-			let co = getRunCentre();
-
-			let x = co[0];
-			let y = co[1];
+			const [avgX, avgY] = getRunCentre();
 
 			// let tooltipOffset = rect.width; // could also have used this.getAttribute('r') * 2)
 			tooltipOffset = 28;
 
 			// run is below the average on the graph, position its tooltip below as well
-			if (yPos > y) {
-
+			if (yPos > avgY) {
 				tooltipX = xPos - tooltipWidth / 2;
 				tooltipY = yPos + tooltipOffset;
 
-				tooltip.style("border", '1px solid ' + color) // for colored borders
+				tooltip.style("border", '3px solid ' + color) // for colored borders
+					.style('border-radius', '10px')
 					.style("left", tooltipX + "px")
-	      			.style("top", tooltipY + "px");
+    			.style("top", tooltipY + "px");
 
-      			// styling for the arrow      			
-      			t.offset({left: tooltipX + tooltipWidth / 2 - triangleSize, top: tooltipY - triangleSize});
-      			t.css({"border-left": "8px solid transparent"});
- 				t.css({"border-right": "8px solid transparent"});
-      			t.css({"border-bottom": "8px solid " + color});
-      			t.css({"border-top": "0px solid transparent"});
+    			// styling for the arrow      			
+    			t.offset({left: tooltipX + tooltipWidth / 2 - triangleSize, top: tooltipY - triangleSize});
+    			t.css({"border-left": "8px solid transparent"});
+					t.css({"border-right": "8px solid transparent"});
+    			t.css({"border-bottom": "8px solid " + color});
+    			t.css({"border-top": "0px solid transparent"});
 			}
 
 			// run is above the average on the graph
 			else {
-				let tooltipX = xPos - tooltipWidth/2
+				let tooltipX = xPos - tooltipWidth / 2
 				let tooltipY = yPos - tooltipOffset - tooltipHeight;
-				let sTop = $('.svg').offset().top; // TODO: this should not be calculated every time
+				const sTop = $('.svg').offset().top; // TODO: this should not be calculated every time
 				
 
 				// tooltip would hit the title, put it on the side instead
 				// TODO: it could still hit the title if the run is slow enough
 				if (tooltipY < sTop) {
-					tooltipY = yPos - tooltipHeight/2;
+					tooltipY = yPos - tooltipHeight / 2;
 					
 					// if run is left of center, put tooltip to the left
-					if (xPos < x) {
+					if (xPos < avgX) {
 						tooltipX = xPos - tooltipOffset - tooltipWidth;
 						
 						// styling for the arrow
-		      			t.offset({left: tooltipX + tooltipWidth - 1, top: tooltipY + tooltipHeight / 2 - triangleSize});
-		      			t.css({"border-left": "8px solid " + color});
+      			t.offset({left: tooltipX + tooltipWidth - 1, top: tooltipY + tooltipHeight / 2 - triangleSize});
+      			t.css({"border-left": "8px solid " + color});
 		 				t.css({"border-right": "0px solid transparent"});
-		      			t.css({"border-bottom": "8px solid transparent"});
-		      			t.css({"border-top": "8px solid transparent"});
+      			t.css({"border-bottom": "8px solid transparent"});
+      			t.css({"border-top": "8px solid transparent"});
 					} 
 					else {
 						tooltipX = xPos + tooltipOffset;
 
 						// styling for the arrow
-		      			t.offset({left: tooltipX - triangleSize, top: tooltipY + tooltipHeight / 2 - triangleSize});
-		      			t.css({"border-left": "0px solid transparent"});
+      			t.offset({left: tooltipX - triangleSize, top: tooltipY + tooltipHeight / 2 - triangleSize});
+      			t.css({"border-left": "0px solid transparent"});
 		 				t.css({"border-right": "8px solid " + color});
-		      			t.css({"border-bottom": "8px solid transparent"});
-		      			t.css({"border-top": "8px solid transparent"});
+      			t.css({"border-bottom": "8px solid transparent"});
+      			t.css({"border-top": "8px solid transparent"});
 					}
 				}
 
 				else {
-
 					// styling for the arrow
-	      			t.offset({left: tooltipX + tooltipWidth / 2 - triangleSize, top: tooltipY + tooltipHeight});
-	      			t.css({"border-left": "8px solid transparent"});
+    			t.offset({left: tooltipX + tooltipWidth / 2 - triangleSize, top: tooltipY + tooltipHeight});
+    			t.css({"border-left": "8px solid transparent"});
 	 				t.css({"border-right": "8px solid transparent"});
-	      			t.css({"border-bottom": "0px solid transparent"});
-	      			t.css({"border-top": "8px solid " + color});
+    			t.css({"border-bottom": "0px solid transparent"});
+    			t.css({"border-top": "8px solid " + color});
 				}
 
 				tooltip.style("border", '1px solid ' + color) // for colored borders
 					.style("left", tooltipX + "px")     
-	      			.style("top", tooltipY + "px");
+    			.style("top", tooltipY + "px");
 			}
 		}
     });
-    $("svg circle, svg polygon").on("mouseout", function(e) {    
+    $("svg circle, svg polygon").on("mouseout", (e) => {    
     	// tooltip.transition()
     	// .duration(100)
     	tooltip.style("opacity", 0);
@@ -224,7 +209,6 @@ d3.csv("https://raw.githubusercontent.com/paulmotz/paulmotz.github.io/master/dat
  * Calculates the centre of all the coordinates of runs (circles and stars) on the page
  * @return {number[]} - the co-ordinates of the centre of the runs in the form {x, y}
  */
-
 function getRunCentre() {
 	let x = 0;
 	let y = 0;
@@ -244,18 +228,15 @@ function getRunCentre() {
  * @param {Object[]} data - running data
  * @return {Object} sortedRuns - contains runs sorted by year and info for each year
  */
-
 function getRuns(data) {
-
-	let runs = [];
+	const runs = [];
 	let colonIndex = 0;
 
-	data.forEach(function(row) {
-
+	data.forEach((row) => {
 		// if I ran that day, grab the relevant info
 		if (row.Dist && row.Date) {
-			let data = {};
-			let timeString = !row["Start Time"] ? '' : ' ' + row["Start Time"];
+			const data = {};
+			const timeString = !row["Start Time"] ? '' : ' ' + row["Start Time"];
 			data.date = new Date(row.Date + timeString);
 			data.dist = +row.Dist;
 			data.duration = +row.Duration;
@@ -268,11 +249,9 @@ function getRuns(data) {
 		}
 	});
 
-	let sortedRuns = runs.sort(
-		function(a, b) {
-			return b.dist - a.dist;
-		}
-	);
+	console.log(runs)
+
+	const sortedRuns = runs.sort((a, b) => b.dist - a.dist);
 
 	// let sortedRuns = sortRuns(runs);
 
@@ -284,21 +263,19 @@ function getRuns(data) {
  * @param {Object[]} runs - running data with only certain fields
  * @return {Object} sortedRuns - contains runs sorted by year and info for each year
  */
-
 function sortRuns(runs) {
-
-	let numRuns = runs.length;
-	let sortedRuns = {'years' : [], 'avgTemp' : {} };
+	const numRuns = runs.length;
+	const sortedRuns = {'years' : [], 'avgTemp' : {} };
 	let thisYear = '0';
 	let year;
 	let temp = 0
 	let tempCount = 0;
 
-	for (let i = 0; i < numRuns; i++) {
-		year = runs[i].date.getFullYear();
+	for (const run of runs) {
+		year = runs.date.getFullYear();
 		if (year !== thisYear) {
-			if (runs[i].temp) {
-				temp += runs[i].temp;
+			if (run.temp) {
+				temp += run.temp;
 				tempCount++;
 				sortedRuns.avgTemp[thisYear] = temp/tempCount;
 			}
@@ -309,10 +286,10 @@ function sortRuns(runs) {
 			sortedRuns[thisYear] = [];
 		}
 
-		sortedRuns[thisYear].push(runs[i]);
+		sortedRuns[thisYear].push(run);
 		
-		if (runs[i].temp) {
-			temp += runs[i].temp;
+		if (run.temp) {
+			temp += run.temp;
 			tempCount++;
 		}
 	}
@@ -329,41 +306,36 @@ function sortRuns(runs) {
  * @param {Object} runs - running data for all years
  * @param {number[]} years - the years for which there is running data
  */
-
 function plotAll(svg, runs, years) {
-
-	runs.forEach(function(row) {
-		let day = row.date.getDate();
-		let month = row.date.getMonth();
-		let year = row.date.getFullYear();
-		let leap = year % 4 === 0 && month > 1 ? 1 : 0;
-		let daysIntoYear = monthDays[month] + day + leap;
+	runs.forEach((row) => {
+		const day = row.date.getDate();
+		const month = row.date.getMonth();
+		const year = row.date.getFullYear();
+		const leap = year % 4 === 0 && month > 1 ? 1 : 0;
+		const daysIntoYear = MONTH_DAYS[month] + day + leap;
 
 		// string representation of data for tooltips
-		let dateString = monthNames[row.date.getMonth()] + ' ' + row.date.getDate() + ' ' + row.date.getFullYear();
-		let minuteString = Math.floor(row.pace / 1).toString();
-		let secondString = Math.round(row.pace % 1 * 60).toString();
-		secondString = secondString.length === 1 ? '0' + secondString : secondString;
-		let paceString = minuteString + ':' + secondString + ' min/km';
+		const dateString = MONTH_NAMES[row.date.getMonth()] + ' ' + row.date.getDate() + ' ' + row.date.getFullYear();
+		const paceString = `${getPaceString(row.pace)} min/km`
 
 		if (!row.race) {
 			svg.append('circle')
 				.attr('class', year)
 				.attr('r', r(row.dist))
 				.attr('fill', getColor(year, years, 0.3))
-				.attr('stroke','rgba(0,0,0,0.3)')
+				.attr('stroke','rgba(0, 0, 0, 0.3)')
 				.attr('cx', x(daysIntoYear))
 				.attr('cy', y(row.pace))
 				.attr('title', 'Date: ' + dateString + '<br>Pace: ' + paceString + '<br>Dist ' + row.dist + ' km');
 		}
 		if (row.race) {
-			let cX = x(daysIntoYear);
-			let cY = y(row.pace);
-			let rS = r(row.dist);
-			let side = rS * Math.cos(18/180*Math.PI); 
-			let pentBisectRad = rS * Math.sin(18/180*Math.PI);
-			let pentCornRad = pentBisectRad / Math.cos(36/180*Math.PI);
-			let star = [{'x':cX, 'y':cY - rS},
+			const cX = x(daysIntoYear);
+			const cY = y(row.pace);
+			const rS = r(row.dist);
+			const side = rS * Math.cos(18/180*Math.PI); 
+			const pentBisectRad = rS * Math.sin(18/180*Math.PI);
+			const pentCornRad = pentBisectRad / Math.cos(36/180*Math.PI);
+			const star = [{'x':cX, 'y':cY - rS},
 						{'x':cX + pentCornRad * Math.sin(36/180*Math.PI), 'y':cY - pentCornRad * Math.cos(36/180*Math.PI)},
 						{'x':cX + side, 'y':cY - pentCornRad * Math.cos(36/180*Math.PI)},
 						{'x':cX + pentCornRad * Math.cos(18/180*Math.PI), 'y':cY + pentCornRad * Math.sin(18/180*Math.PI)},
@@ -374,7 +346,7 @@ function plotAll(svg, runs, years) {
 						{'x':cX - side, 'y':cY - pentCornRad * Math.cos(36/180*Math.PI)},
 						{'x':cX - pentCornRad * Math.sin(36/180*Math.PI), 'y':cY - pentCornRad * Math.cos(36/180*Math.PI)}];
 
-			let starString = star.map(function(p) {
+			const starString = star.map((p) => {
 				return [p.x,p.y].join(',');
 			}).join(', ');
 
@@ -383,14 +355,14 @@ function plotAll(svg, runs, years) {
 				.attr('points', starString)
 				.attr('fill', getColor(year, years, 0.3))
 				.attr('stroke','rgba(0,0,0,0.9)')
-				.attr('title', 'Date: ' + dateString + '<br>Pace: ' + paceString + '<br>Dist: ' + row.dist + ' km<br>' + row.race + ' ' + year);
+				.attr('title', `Date: ${dateString}<br>Pace: ${paceString}<br>Dist: ${row.dist} km<br> ${row.race} ${year}`);
 		}		
 	});	
 
 	$('#years-to-display').html('<h4 class="form-header">Show data from the following years:</h4>');
 
-	for (let index in years) {
-		let year = years[index];
+	for (const index in years) {
+		const year = years[index];
 		if (index > 0 && index % 3 === 0) {
 			$('#years-to-display').append('<br>');
 		}
@@ -411,18 +383,17 @@ function plotAll(svg, runs, years) {
  * Displays the proper runs based on the checkboxes and radio button selected
  * @param {number[]} years - the years for which there is running data
  */
-
 function displayRuns(years) {
-	let checkboxes = $('input[type=checkbox]');
-	let checkedYears = [];
-	for (let checkbox of checkboxes) {
+	const checkboxes = $('input[type=checkbox]');
+	const checkedYears = [];
+	for (const checkbox of checkboxes) {
 		if (checkbox.checked) {
 			checkedYears.push(checkbox.name);
 		}
 	}
-	let checkedRadio = $('input[name=radioruns]:checked', '#races-to-display').val();
-	for (let year of years) {
-		if (checkedYears.indexOf(String(year)) !== -1) {
+	const checkedRadio = $('input[name=radioruns]:checked', '#races-to-display').val();
+	for (const year of years) {
+		if (checkedYears.includes(String(year))) {
 			if (checkedRadio === 'race') {
 				$('circle.' + year).hide();
 				$('polygon.' + year).show();
@@ -450,11 +421,10 @@ function displayRuns(years) {
  * @param {number} opacity - the desired opacity of the color
  * @return {string} color
  */
-
 function getColor(year, years, opacity) {
-	let yearIndex = years.indexOf(Number(year)); // ensure year is a number and not a string
-	let colorValues = [[139 ,69, 19], [256, 0, 0], [0, 256, 0], [0, 0, 256], [256, 256, 0], [256, 0, 256], [0, 256, 256], [256, 256, 256]];
-	let colorString = 'rgba(' + colorValues[yearIndex].join() + ',' + opacity + ')';
+	const yearIndex = years.indexOf(Number(year)); // ensure year is a number and not a string
+	const colorValues = [[139 ,69, 19], [256, 0, 0], [0, 256, 0], [0, 0, 256], [256, 256, 0], [256, 0, 256], [0, 256, 256], [256, 256, 256]];
+	const colorString = `rgba(${colorValues[yearIndex].join()},${opacity})`;
 	return colorString;
 }
 
@@ -462,10 +432,21 @@ function getColor(year, years, opacity) {
  * Checks whether the portfolio dropdown is expanded, method returns a string rather than a boolean, so need to check whether string is 'true' or 'false'
  * @return {Boolean} whether the dropdown is expanded or not
  */
-
 function dropdownExpanded() {
 	if (document.getElementById('portfolio-dropdown').getAttribute('aria-expanded') === 'true') return true;
 	return false;
+}
+
+/**
+ * Converts a decimal number to a pace string in the format mm:ss
+ * @param {number} pace - decimal representation of a run's pace 
+ * @return {string} paceString
+ */
+function getPaceString(pace) {
+	const min = Math.floor(pace / 1);
+	const sec = Math.round(pace % 1 * 60);
+	const filler = sec < 10 ? ':0' : ':';
+	return min + filler + sec;
 }
 
 /**
